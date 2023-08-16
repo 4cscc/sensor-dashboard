84308a428cae0b2e94634845ab02035e6e686071
F. Stewart <franklin.stewart@nau.edu>
2023-08-16 16:15:22 -0600

    FEAT: Intial implementation of scalable plots

--- sensor-dash.py
+++ sensor-dash.py
@@ -25,9 +25,28 @@ app = dash.Dash(__name__)
 app.layout = html.Div([
     html.Div(children='show data'),
     dash_table.DataTable(data=aq_df.to_dict('records'), page_size=10),
-    dcc.Graph(figure=px.scatter(aq_df, x='timestamp', y='co2')),
+    dcc.Graph(figure=px.scatter(aq_df, x='timestamp', y='co2', animation_frame="timestamp")),
     dcc.Graph(figure=px.scatter(aq_df, x='timestamp', y='ethanol')),
+    # dcc.Graph(id="scatter-plot"),
+    # dcc.RangeSlider(
+    #     id='range-slider',
+    #     min=aq_df["timestamp"].min(), max=aq_df["timestamp"].max(),
+    #     marks={0: '0', 2.5: '2.5'},
+    #     value=[aq_df.iloc[-101]["timestamp"], aq_df.iloc[-1]["timestamp"]]
+    # )
 ])
+
+# @app.callback(
+#     Output("scatter-plot", "figure"), 
+#     Input("range-slider", "value"))
+# def update_bar_chart(slider_range):
+#     df = aq_df # replace with your own data source
+#     low, high = slider_range
+#     mask = (df['timestamp'] > low) & (df['timestamp'] < high)
+#     fig = px.scatter(
+#         df[mask], x="timestamp", y="co2", 
+# )
+#     return fig
 
 if __name__ == '__main__':
     app.run()
